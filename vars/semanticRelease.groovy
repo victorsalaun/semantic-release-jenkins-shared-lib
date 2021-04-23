c75bcc9394bd762f106e21a260a43777d0066733
Victor SALAUN <vsalaun@talend.com>
2021-04-24 00:16:26 +0200

    init shared lib

--- vars/semanticRelease.groovy
+++ vars/semanticRelease.groovy
@@ -1,33 +1,28 @@
-
 def call(Map args = [:]) {
-    stage("Semantic release") {
-        kubernetes {
-            label "semantic-release"
-            yaml """
-                apiVersion: v1
-                kind: Pod
-                spec:
-                  containers:
-                  - name: container
-                    image: artifactory.datapwn.com/docker-io-remote/node
-                    command:
-                    - cat
-                    tty: true
-                  imagePullSecrets:
-                  - talend-registry
-            """
-        }
-        environment {
-            GH_TOKEN  = credentials('continuous-delivery')
-        }
-        container("container") {
-            script {
-                echo "semantic-release"
-                sh """
-                    npm i -D semantic-release @semantic-release/changelog @semantic-release/git
-                    npx semantic-release
-                """
+
+    withCredentials([string(credentialsId: 'continuous-delivery', variable: 'GH_TOKEN')]) {
+        podTemplate(yaml: """\
+            apiVersion: v1
+            kind: Pod
+            spec:
+              containers:
+              - name: container
+                image: artifactory.datapwn.com/docker-io-remote/node
+                command:
+                - cat
+                tty: true
+              imagePullSecrets:
+              - talend-registry
+            """.stripIndent()) {
+            node("semantic-release") {
+                container('container') {
+                    sh """
+                        npm i -D semantic-release @semantic-release/changelog @semantic-release/git
+                        npx semantic-release
+                    """
+                }
             }
         }
     }
+
 }
